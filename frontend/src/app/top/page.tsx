0996cd4aa99e339c93c4802c07a59642a263ca0b
amami <amami_gt0403@yahoo.co.jp>
2025-10-11 21:11:37 +0900

    feat: ログイン後のトップページを作成

---
+++ frontend/src/app/top/page.tsx
@@ -0,0 +1,16 @@
+import { getProfileData } from "./_lib/queries";
+
+export default async function TopPage() {
+  const profileData = await getProfileData();
+
+  if ("error" in profileData) {
+    return <p>{profileData.error}</p>;
+  }
+
+  return (
+    <div>
+      <p>{profileData.name}</p>
+      <p>{profileData.supabase_uid}</p>
+    </div>
+  );
+}
