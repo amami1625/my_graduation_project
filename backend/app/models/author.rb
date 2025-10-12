fc4f6a66a1289c3002f0cd351d2e9c74fb71da01
amami <amami_gt0403@yahoo.co.jp>
2025-10-12 14:08:13 +0900

    feat: 各モデルにアソシエーションを設定

--- backend/app/models/author.rb
+++ backend/app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ApplicationRecord
   belongs_to :user
+  has_many :book_authors, dependent: :destroy
+  has_many :books, through: :book_authors
 
   validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 100 }
 end
--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :category, optional: true
+  has_many :book_authors, dependent: :destroy
+  has_many :authors, through: :book_authors
 
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
   validates :rating, inclusion: { in: 1..5 }, allow_nil: true
