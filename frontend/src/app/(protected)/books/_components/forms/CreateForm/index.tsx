a12211d75bfaff2e1085987696ff7b1f71d4511d
amami <amami_gt0403@yahoo.co.jp>
2025-10-13 17:43:06 +0900

    feat: 著者を選択する項目を追加

--- frontend/src/app/(protected)/books/_components/forms/CreateForm/index.tsx
+++ frontend/src/app/(protected)/books/_components/forms/CreateForm/index.tsx
@@ -1,7 +1,13 @@
+import { getAuthors } from "@/app/(protected)/authors/_lib/queries";
 import CreateBookClient from "./CreateBookClient";
 
 // TODO: Tag機能を実装したらTagsを追加する
-// TODO: Author機能を実装したらAuthorsを追加する
 export default async function CreateForm() {
-  return <CreateBookClient />;
+  const authors = await getAuthors();
+
+  if ("error" in authors) {
+    return <div className="text-red-500">著者情報の取得に失敗しました</div>;
+  }
+
+  return <CreateBookClient authors={authors}/>;
 }
