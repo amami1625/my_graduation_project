22a1c53a8589bd5cfa98e2d4f7ac8602fc8ad3bb
amami <amami_gt0403@yahoo.co.jp>
2025-10-15 14:52:59 +0900

    refactor: 不要になった useFormStatus を削除

--- frontend/src/app/(protected)/books/_components/forms/CreateForm/CreateBookClient.tsx
+++ frontend/src/app/(protected)/books/_components/forms/CreateForm/CreateBookClient.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useFormStatus } from "react-dom";
 import BookForm from "../BookForm";
 import { createBook } from "@/app/(protected)/books/_lib/actions";
 import { Author } from "@/app/(protected)/authors/types";
@@ -17,15 +16,12 @@ export default function CreateBookClient({
   categories = [],
   authors = [],
 }: CreateBookClientProps) {
-  const { pending } = useFormStatus();
-
   return (
     <BookForm
       authors={authors}
       action={createBook}
       categories={categories}
       submitLabel="作成"
-      loading={pending}
     />
   );
 }
