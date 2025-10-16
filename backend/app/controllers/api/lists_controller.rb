fb522ee75cb33799d7ffa45c163cfb65bd868337
amami <amami_gt0403@yahoo.co.jp>
2025-10-16 21:48:19 +0900

    feat: Lists コントローラーを作成

---
+++ backend/app/controllers/api/lists_controller.rb
@@ -0,0 +1,21 @@
+class Api::ListsController < Api::ApplicationController
+  def index
+    lists = current_user.lists.order(created_at: :desc)
+    render json: lists
+  end
+
+  def create
+    list = current_user.lists.build(list_params)
+    if list.save
+      render json: list, status: :created
+    else
+      render json: { errors: list.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :description, :user_id, :public)
+  end
+end
