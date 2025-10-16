2f19184d129d4975de1375453c1e31f2657f901b
amami <amami_gt0403@yahoo.co.jp>
2025-10-16 21:58:26 +0900

    fix: 重複した記述を削除

--- backend/app/models/list.rb
+++ backend/app/models/list.rb
@@ -1,7 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 100 }
-  validates :name, length: { maximum: 255 }
+  validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
   validates :public, inclusion: { in: [true, false] }
 end
