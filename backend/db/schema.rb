1098545149374390b36b3e35e932efbabbac1f55
amami <amami_gt0403@yahoo.co.jp>
2025-10-16 21:46:55 +0900

    feat: List モデルを作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_12_045157) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_16_103617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_12_045157) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "supabase_uid", null: false
     t.string "name", null: false
@@ -69,4 +79,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_12_045157) do
   add_foreign_key "books", "categories"
   add_foreign_key "books", "users"
   add_foreign_key "categories", "users"
+  add_foreign_key "lists", "users"
 end
