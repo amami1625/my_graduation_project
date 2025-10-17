74d5bc34798318ef88d4a5df8ada5d0fd957f346
amami <amami_gt0403@yahoo.co.jp>
2025-10-17 22:06:35 +0900

    feat: ListBooks とのアソシエーションを設定

--- backend/app/models/list.rb
+++ backend/app/models/list.rb
@@ -1,5 +1,7 @@
 class List < ApplicationRecord
   belongs_to :user
+  has_many :list_books, dependent: :destroy
+  has_many :books, through: :list_books
 
   validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
   validates :public, inclusion: { in: [true, false] }
