18a53330c843b9defa753ad0992e20851a26e3e0
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-19 21:07:42 +0900

    feat: リストからデータを削除する関数を追加

--- frontend/src/app/(protected)/listBooks/_lib/actions.ts
+++ frontend/src/app/(protected)/listBooks/_lib/actions.ts
@@ -1,8 +1,8 @@
 "use server";
 
-import { authenticatedRequest } from "@/supabase/dal";
 import { revalidatePath } from "next/cache";
-import { ListBookFormData } from "../_types";
+import { authenticatedRequest } from "@/supabase/dal";
+import { ListBook, ListBookFormData } from "@/app/(protected)/listBooks/_types";
 
 export async function addListBook(
   formData: ListBookFormData
@@ -14,6 +14,31 @@ export async function addListBook(
     });
 
     revalidatePath(`/lists/${formData.list_id}`);
+    return { success: true };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message };
+    } else {
+      return { error: "不明なエラーが発生しました" };
+    }
+  }
+}
+
+export async function removeListBook(
+  formData: Partial<ListBook>
+): Promise<{ success: true } | { error: string }> {
+  try {
+    await authenticatedRequest(`/list_books/${formData.id}`, {
+      method: "DELETE",
+    });
+
+    if (formData.list_id) {
+      revalidatePath(`/lists/${formData.list_id}`);
+    }
+
+    if (formData.book_id) {
+      revalidatePath(`/books/${formData.book_id}`);
+    }
     return { success: true };
   } catch (error) {
     if (error instanceof Error) {
