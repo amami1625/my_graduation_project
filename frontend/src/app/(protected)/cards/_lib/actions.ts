06158900c151fe25d926124b111951df36777c9f
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-23 14:40:26 +0900

    feat: カードの作成を行う関数を追加

---
+++ frontend/src/app/(protected)/cards/_lib/actions.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { CardFormData } from '@/app/(protected)/cards/_types';
+import { authenticatedRequest } from '@/supabase/dal';
+
+export const createCard = async (
+  formData: CardFormData,
+): Promise<{ success: true } | { error: string }> => {
+  try {
+    await authenticatedRequest(`/books/${formData.book_id}/cards`, {
+      method: 'POST',
+      body: JSON.stringify({ card: formData }),
+    });
+
+    revalidatePath(`/books/${formData.book_id}`);
+    return { success: true };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message };
+    } else {
+      return { error: '不明なエラーが発生しました' };
+    }
+  }
+};
