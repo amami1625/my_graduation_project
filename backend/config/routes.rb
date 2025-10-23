138f824196d446b7996b33f88238f54b17972243
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-23 12:00:14 +0900

    feat: Cards コントローラーを作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # API routes
   namespace :api do
     get "hello/index", to: "hello#index"
-    resources :books, only: %i[index create update show destroy]
+    resources :books, only: %i[index create update show destroy] do
+      resources :cards, only: %i[create]
+    end
     resources :lists, only: %i[index create update show destroy]
     resources :list_books, only: %i[create destroy]
     resources :authors, only: %i[index create]
