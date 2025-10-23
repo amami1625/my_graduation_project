b4aa4c61bb7cbfdf8fb18b8b2fd177e293045d3d
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-23 11:45:03 +0900

    feat: Card モデルを作成しアソシエーションを設定

---
+++ backend/db/migrate/20251023023239_create_cards.rb
@@ -0,0 +1,11 @@
+class CreateCards < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cards do |t|
+      t.references :book, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :content, null: false
+
+      t.timestamps
+    end
+  end
+end
