9fe8d0eced837f58b63e8d14c361b9b0e881d41c
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-25 20:05:38 +0900

    feat: カード詳細データのスキーマを作成

--- frontend/src/app/(protected)/cards/_types/index.ts
+++ frontend/src/app/(protected)/cards/_types/index.ts
@@ -1,8 +1,10 @@
 export {
   type Card,
+  type CardDetail,
   type CardFormData,
   type CardList,
   cardSchema,
+  cardDetailSchema,
   cardFormSchema,
   cardListSchema,
 } from '@/schemas/card';
